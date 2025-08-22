c927f0cf8905e6c8e83459c962e2b8cf42112ac5
FelipeSM03 <00001128560276SP@aluno.educacao.sp.gov.br>
2025-08-22 08:30:30 -0300

    llp

---
+++ s17/a3.py
@@ -0,0 +1,24 @@
+numero_pessoas = int(input("digite o numero de pessoas: "))
+tipo_reunião = int(input("digite o tipo de reuniao: "))
+executiva = int(input)
+normal= int(input)
+
+
+if numero_pessoas >= 1 and numero_pessoas <= 5 :
+     print("sala pequena")
+elif numero_pessoas >= 6 and numero_pessoas <= 15 :
+     print("sala média")
+elif numero_pessoas >= 15 :
+     print("sala grande")
+else:
+        print("não há reunião hoje")
+
+
+
+
+
+
+
+
+
+
---
+++ s18/a2.py
@@ -0,0 +1,18 @@
+link3 = int(input("numero de pessoas que clicaram no link3 [1, 1000]: "))
+while link3 < 0 or link3 > 1000:
+    link3 = int(input("numero de pessoas que clicaram no link3 [1, 1000]: "))
+link2 = 2 * link3 
+link1 = 2 * link2
+print(f"link1 = {link1}")
+
+
+
+
+
+
+
+
+
+
+
+
