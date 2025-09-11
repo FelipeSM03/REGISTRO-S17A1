05a6f21710d4c77329f398d99089b2bf5faab868
FelipeSM03 <00001128560276SP@aluno.educacao.sp.gov.br>
2025-09-11 09:12:06 -0300

    S19a1

---
+++ s19/a1.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+for i in range(1, n + 1):
+    num = i
+    num_quadrado = i * i
+    num_cubo = i * i * i
+
+    print(f"{num} {num_quadrado} {num_cubo}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
